3af068f226d1f52f921d7f713cc897da990a7867
bksrinu546@gmail.com <bksrinu546@gmail.com>
2022-01-14 17:18:08 +0530

    Section 2 - End of section 2 API, EFcore, and Automapper

---
+++ StudentAdminPortal.API/Profiles/AutomapperProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using StudentAdminPortal.API.DomainModels;
+using DataModels= StudentAdminPortal.API.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Profiles
+{
+    public class AutomapperProfiles: Profile
+    {
+        public AutomapperProfiles()
+        {
+            CreateMap<DataModels.Student, Student>().ReverseMap();
+            CreateMap<DataModels.Gender, Gender>().ReverseMap();
+            CreateMap<DataModels.Address, Address>().ReverseMap();
+        }
+    }
+}
